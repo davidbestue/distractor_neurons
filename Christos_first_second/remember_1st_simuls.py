8edcb7db2a91512b643ea6ff3df1317516913b04
davidbestue <davidsanchezbestue@hotmail.com>
2020-01-31 15:37:30 +0100

    Update remember_1st_simuls.py

--- Christos_first_second/remember_1st_simuls.py
+++ Christos_first_second/remember_1st_simuls.py
@@ -23,22 +23,30 @@ fii=1.
 
 
 n_simuls=200
-# numcores = multiprocessing.cpu_count() 
-# print('Number cores: '+ str(numcores))
-# results_1st_close_off = Parallel(n_jobs = numcores)(delayed(model)(totalTime=3000, targ_onset_1=50, targ_onset_2=500, angle_target_i=90, presentation_period=100,
-#            angle_separation=49, tauE=20, tauI=10,  n_stims=2, I0E=0.1, I0I=0.5, 
-#            GEE=0.068*fee,
-#            GII= 0.13*fei,
-#            GEI=0.13*fie,
-#            GIE=0.042*fii, 
-#            sigE=10., sigI=5., 
-#            kappa_E=45, 
-#            kappa_I=0.5, #OFF
-#            kappa_stim=40., N=512, stim_strengthE=9.20, stim_strengthI=0.,
-#            plot_connectivity=False, plot_rate=False, plot_hm=False , plot_fit=False)  for n in range(n_simuls)) 
+
+
 
 
 ### Close: off
+## Paralel
+numcores = multiprocessing.cpu_count() 
+print('Number cores: '+ str(numcores))
+results_1st_close_off = Parallel(n_jobs = numcores)(delayed(model)(totalTime=3000, targ_onset_1=50, targ_onset_2=500, angle_target_i=90, presentation_period=100,
+           angle_separation=49, tauE=20, tauI=10,  n_stims=2, I0E=0.1, I0I=0.5, 
+           GEE=0.068*fee,
+           GII= 0.13*fei,
+           GEI=0.13*fie,
+           GIE=0.042*fii, 
+           sigE=10., sigI=5., 
+           kappa_E=45, 
+           kappa_I=0.5, #OFF
+           kappa_stim=40., N=512, stim_strengthE=9.20, stim_strengthI=0.,
+           plot_connectivity=False, plot_rate=False, plot_hm=False , plot_fit=False)  for n in range(n_simuls)) 
+
+np.mean([abs(results_1st_close_off[i][1]) for i in range(len(results_1st_close_off))]) ###¿¿¿16???
+
+
+#serie
 res_off=[]
 
 for i in range(n_simuls):
@@ -59,7 +67,7 @@ for i in range(n_simuls):
 
 
 ###
-print('abs error OFF, close: ' + str(round(np.mean([abs(res_off[i]) for i in range(len(res_off))]),2) ) )
+print('abs error OFF, close: ' + str(round(np.mean([abs(res_off[i]) for i in range(len(res_off))]),2) ) ) ## 16
 
 OFF = pd.DataFrame(res_off)
 OFF['stimul']='ON' 
@@ -67,6 +75,25 @@ OFF['position']='close'
 
 
 ### Close: on
+
+## Paralel
+numcores = multiprocessing.cpu_count() 
+print('Number cores: '+ str(numcores))
+results_1st_close_on = Parallel(n_jobs = numcores)(delayed(model)(totalTime=3000, targ_onset_1=50, targ_onset_2=500, angle_target_i=90, presentation_period=100,
+           angle_separation=49, tauE=20, tauI=10,  n_stims=2, I0E=0.1, I0I=0.5, 
+           GEE=0.068*fee,
+           GII= 0.13*fei,
+           GEI=0.13*fie,
+           GIE=0.042*fii, 
+           sigE=10., sigI=5., 
+           kappa_E=45, 
+           kappa_I=0.35, #ON
+           kappa_stim=40., N=512, stim_strengthE=9.20, stim_strengthI=0.,
+           plot_connectivity=False, plot_rate=False, plot_hm=False , plot_fit=False)  for n in range(n_simuls)) 
+
+np.mean([abs(results_1st_close_on[i][1]) for i in range(len(results_1st_close_on))]) ###¿¿¿18.31???
+
+## Serie
 res_on=[]
 
 for i in range(n_simuls):
@@ -87,7 +114,7 @@ for i in range(n_simuls):
 
 
 ###
-print('abs error ON, close: ' + str(round(np.mean([abs(res_on[i]) for i in range(len(res_on))]),2) ) )
+print('abs error ON, close: ' + str(round(np.mean([abs(res_on[i]) for i in range(len(res_on))]),2) ) ) ##18.31
 
 
 
@@ -120,7 +147,7 @@ for i in range(n_simuls):
 
 
 ###
-print('abs error OFF, close: ' + str(round(np.mean([abs(res_off_f[i]) for i in range(len(res_off_f))]),2) ) )
+print('abs error OFF, far: ' + str(round(np.mean([abs(res_off_f[i]) for i in range(len(res_off_f))]),2) ) )
 
 OFF_f = pd.DataFrame(res_off_f)
 OFF_f['stimul']='ON' 
@@ -148,7 +175,7 @@ for i in range(n_simuls):
 
 
 ###
-print('abs error ON, close: ' + str(round(np.mean([abs(res_on_f[i]) for i in range(len(res_on_f))]),2) ) )
+print('abs error ON, far: ' + str(round(np.mean([abs(res_on_f[i]) for i in range(len(res_on_f))]),2) ) )
 
 
 
