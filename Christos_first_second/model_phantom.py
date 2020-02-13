1607fb752e171855ca8be6b94b738228cd104fff
davidbestue <davidsanchezbestue@hotmail.com>
2020-02-13 13:33:28 +0100

    update

--- Christos_first_second/model_phantom.py
+++ Christos_first_second/model_phantom.py
@@ -197,7 +197,7 @@ def model(totalTime, targ_onset_1, targ_onset_2, presentation_period, angle_targ
     f = lambda x : x*x*(x>0)*(x<1) + reshape(array([cmath.sqrt(4*x[i]-3) for i in range(0, len(x))]).real, (N,1)) * (x>=1)
 
     background_s= I0E* ones((N,1))
-    background_on = phantom_st * ones((N,1))
+    background_on = (I0E+phantom_st) * ones((N,1))
     background = background_s 
     ### diferential equations
     for i in range(0, nsteps):
