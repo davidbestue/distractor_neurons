1c92cd49dc98fadfe36ae349da70d9ba97e18747
davidbestue <davidsanchezbestue@hotmail.com>
2020-03-09 10:35:38 +0100

    update

--- Christos_first_second/model_phantom_DB.py
+++ Christos_first_second/model_phantom_DB.py
@@ -218,6 +218,7 @@ def model(totalTime, targ_onset_1, targ_onset_2, presentation_period, angle_targ
     background_on = (I0E+phantom_st) * ones((N,1))
     background = background_s 
     ##noise
+    numcores = multiprocessing.cpu_count()
     noisepopE = np.array( Parallel(n_jobs = numcores)(delayed(Uhlenbeck)(totaltime=totalTime, dt=dt, kappa=k_noise)  for n in range(N)) )
     noisepopI = np.array( Parallel(n_jobs = numcores)(delayed(Uhlenbeck)(totaltime=totalTime, dt=dt, kappa=k_noise)  for n in range(N)) )
     ### diferential equations
