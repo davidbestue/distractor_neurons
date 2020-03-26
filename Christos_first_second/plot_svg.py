594447e99ed08b2581edbb79557a57cda4f0c171
davidbestue <davidsanchezbestue@hotmail.com>
2020-03-26 19:47:44 +0100

    update

--- Christos_first_second/plot_svg.py
+++ Christos_first_second/plot_svg.py
@@ -210,19 +210,38 @@ fii=1.08
 # plt.savefig("2_close_off.pdf")
 
 
-on_far_2= model(totalTime=3000, targ_onset_1=100, targ_onset_2=1000, angle_target_i=90, presentation_period=100,
-           angle_separation=170, tauE=20, tauI=10,  n_stims=2, I0E=0.05, I0I=0.5, 
+# on_far_2= model(totalTime=3000, targ_onset_1=100, targ_onset_2=1000, angle_target_i=90, presentation_period=100,
+#            angle_separation=170, tauE=20, tauI=10,  n_stims=2, I0E=0.05, I0I=0.5, 
+#            GEE=0.068*fee,
+#            GII= 0.13*fei,
+#            GEI=0.13*fie,
+#            GIE=0.042*fii, 
+#            sigE=7., sigI=5., k_noise=0.6,             
+#            kappa_E=45, 
+#            kappa_I=0.3, 
+#            kappa_stim=40., N=512, stim_strengthE=9.4, stim_strengthI=0.,
+#            plot_connectivity=False, plot_rate=False, plot_hm=False , plot_fit=False, 
+#            phantom_st=1.2, phantom_onset=50000, phnatom_duration=100)
+
+# hemap2f(on_far_2)
+# plt.show(block=False)
+# plt.savefig("2_far_on.pdf")
+
+
+
+off_far_2= model(totalTime=3000, targ_onset_1=100, targ_onset_2=1000, angle_target_i=90, presentation_period=100,
+           angle_separation=170, tauE=20, tauI=10,  n_stims=2, I0E=-2., I0I=0.5, 
            GEE=0.068*fee,
            GII= 0.13*fei,
            GEI=0.13*fie,
            GIE=0.042*fii, 
-           sigE=7., sigI=5., k_noise=0.6,             
+           sigE=7., sigI=5.7, k_noise=0.6,             
            kappa_E=45, 
            kappa_I=0.3, 
            kappa_stim=40., N=512, stim_strengthE=9.4, stim_strengthI=0.,
            plot_connectivity=False, plot_rate=False, plot_hm=False , plot_fit=False, 
            phantom_st=1.2, phantom_onset=50000, phnatom_duration=100)
 
-hemap2f(on_far_2)
+hemap2f(off_far_2)
 plt.show(block=False)
-#plt.savefig("2_far_on.pdf")
+plt.savefig("2_far_off.pdf")
