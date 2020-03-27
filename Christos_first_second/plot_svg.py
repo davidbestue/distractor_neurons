e0f2ba69d6c59d733472d12551f1e308a2b2e002
davidbestue <davidsanchezbestue@hotmail.com>
2020-03-27 13:11:57 +0100

    update

--- Christos_first_second/plot_svg.py
+++ Christos_first_second/plot_svg.py
@@ -86,6 +86,26 @@ def hemap2f(an):
 
 
 
+def hemap_p(an):
+    RE_sorted=flipud(an[4])
+    sns.heatmap(RE_sorted, cmap=pal_cyan, vmin=0, vmax=17,  cbar=True)
+    plt.gca().set_ylabel('')
+    plt.gca().set_xlabel('')
+    plt.gca().set_title('')
+    plt.gca().plot([500, 900], [p_targ2, p_targ2], ls='--', color =ltc, linewidth=lw_t) ## flipped, so it is p_target 
+    #plt.gca().set_xticks([])
+    plt.gca().set_xticks([0,an[4].shape[1]/2, an[4].shape[1]])
+    plt.gca().set_xticklabels(['0', str(an[4].shape[1]), str(an[4].shape[1]*2)], rotation=0)
+    #axn.set_xticks([0,simul[4].shape[1]/2, simul[4].shape[1]])
+    plt.gca().set_xticklabels(['0s', str(an[4].shape[1]/1000) + 's', str( int(an[4].shape[1]*2/1000) ) + 's'], rotation=0)
+    plt.gca().set_yticks([0, N/4, N/2,  3*N/4, N ])
+    #plt.gca().set_yticklabels(['0','90','180', '270', '360'])
+    plt.gca().set_yticklabels(['0','','$^\pi$', '', '2$^\pi$'])
+    plt.gca().set_xlabel('time (ms)');
+    plt.gca().set_ylabel('neuron ($^\circ$)');
+
+#
+
 
 fee=1
 fei=1
@@ -229,19 +249,59 @@ fii=1.08
 
 
 
-off_far_2= model(totalTime=3000, targ_onset_1=100, targ_onset_2=1000, angle_target_i=90, presentation_period=100,
-           angle_separation=170, tauE=20, tauI=10,  n_stims=2, I0E=-2., I0I=0.5, 
+# off_far_2= model(totalTime=3000, targ_onset_1=100, targ_onset_2=1000, angle_target_i=90, presentation_period=100,
+#            angle_separation=170, tauE=20, tauI=10,  n_stims=2, I0E=-2., I0I=0.5, 
+#            GEE=0.068*fee,
+#            GII= 0.13*fei,
+#            GEI=0.13*fie,
+#            GIE=0.042*fii, 
+#            sigE=7., sigI=5.7, k_noise=0.6,             
+#            kappa_E=45, 
+#            kappa_I=0.3, 
+#            kappa_stim=40., N=512, stim_strengthE=9.4, stim_strengthI=0.,
+#            plot_connectivity=False, plot_rate=False, plot_hm=False , plot_fit=False, 
+#            phantom_st=1.2, phantom_onset=50000, phnatom_duration=100)
+
+# hemap2f(off_far_2)
+# plt.show(block=False)
+# plt.savefig("2_far_off.pdf")
+
+
+fee=0.94
+fei=0.92
+fie=1.14
+fii=1.08
+
+# phantom_on= model(totalTime=2000, targ_onset_1=10000, targ_onset_2=1000, angle_target_i=90, presentation_period=100,
+#            angle_separation=0, tauE=20, tauI=10,  n_stims=2, I0E=0.05, I0I=0.5, 
+#            GEE=0.068*fee,
+#            GII= 0.13*fei,
+#            GEI=0.13*fie,
+#            GIE=0.042*fii, 
+#            sigE=7., sigI=5., k_noise=0.6,             
+#            kappa_E=45, 
+#            kappa_I=0.3, 
+#            kappa_stim=40., N=512, stim_strengthE=9.4, stim_strengthI=0.,
+#            plot_connectivity=False, plot_rate=False, plot_hm=False , plot_fit=False, 
+#            phantom_st=1.2, phantom_onset=100, phantom_on = 'on', phnatom_duration=300)
+
+# hemap_p(phantom_on)
+# plt.show(block=False)
+# plt.savefig("phantom_on.pdf")
+
+phantom_off= model(totalTime=2000, targ_onset_1=10000, targ_onset_2=1000, angle_target_i=90, presentation_period=100,
+           angle_separation=0, tauE=20, tauI=10,  n_stims=2, I0E=-2., I0I=0.5, 
            GEE=0.068*fee,
            GII= 0.13*fei,
            GEI=0.13*fie,
            GIE=0.042*fii, 
-           sigE=7., sigI=5.7, k_noise=0.6,             
+           sigE=7., sigI=5., k_noise=0.6,             
            kappa_E=45, 
            kappa_I=0.3, 
            kappa_stim=40., N=512, stim_strengthE=9.4, stim_strengthI=0.,
            plot_connectivity=False, plot_rate=False, plot_hm=False , plot_fit=False, 
-           phantom_st=1.2, phantom_onset=50000, phnatom_duration=100)
+           phantom_st=1.2, phantom_onset=100, phantom_on = 'on', phnatom_duration=500)
 
-hemap2f(off_far_2)
+hemap_p(phantom_off)
 plt.show(block=False)
-plt.savefig("2_far_off.pdf")
+# plt.savefig("phantom_on.pdf")
