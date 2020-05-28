88c6dde443eb141ecff6b6abbc2dd685ddaa5843
David Bestue <davidsanchezbestue@hotmail.com>
2020-05-28 12:09:09 +0200

    create

---
+++ Christos_first_second/R2_high_firing_simuls.py
@@ -0,0 +1,117 @@
+from model_phantom_DB import *
+#from model_phantom import *
+
+#from linares_plot import * 
+
+n_simuls=10
+numcores = multiprocessing.cpu_count() -2
+print('Number cores: '+ str(numcores))
+
+
+fee=0.94
+fei=1.9
+fie=0.75
+fii=1.50
+
+
+ON_2_far = Parallel(n_jobs = numcores)(delayed(model)(totalTime=3000, targ_onset_1=100, targ_onset_2=1000, angle_target_i=90, presentation_period=100,
+           angle_separation=170, tauE=20, tauI=10,  n_stims=2, I0E=0.05, I0I=0.5, 
+           GEE=0.068*fee,
+           GII= 0.13*fii,
+           GEI=0.13*fei,
+           GIE=0.042*fie, 
+           sigE=7., sigI=5., k_noise=0.6,            
+           kappa_E=45, 
+           kappa_I=0.3, 
+           kappa_stim=40., N=512, stim_strengthE=9.4, stim_strengthI=0.,
+           plot_connectivity=False, plot_rate=False, plot_hm=False , plot_fit=False, 
+           phantom_st=1.2, phantom_onset=50000, phnatom_duration=100)  for n in range(n_simuls)) 
+
+
+
+OFF_2_far = Parallel(n_jobs = numcores)(delayed(model)(totalTime=3000, targ_onset_1=100, targ_onset_2=1000, angle_target_i=90, presentation_period=100,
+           angle_separation=170, tauE=20, tauI=10,  n_stims=2, I0E=-2., I0I=0.5, 
+           GEE=0.068*fee,
+           GII= 0.13*fii,
+           GEI=0.13*fei,
+           GIE=0.042*fie, 
+           sigE=7., sigI=5., k_noise=0.6,            
+           kappa_E=45, 
+           kappa_I=0.3, 
+           kappa_stim=40., N=512, stim_strengthE=9.4, stim_strengthI=0.,
+           plot_connectivity=False, plot_rate=False, plot_hm=False , plot_fit=False, 
+           phantom_st=1.2, phantom_onset=50000, phnatom_duration=100)  for n in range(n_simuls)) 
+
+
+
+err2_on_f = pd.DataFrame([ON_2_far[i][2] for i in range(len(ON_2_far))])
+err2_on_f.columns=['err']
+err2_on_f['abs_err']=abs(err2_on_f['err'])
+err2_on_f['stimulation']='ON'
+err2_on_f['distance']='far'
+err2_on_f['order']='2nd'
+err2_on_f.to_excel('/home/david/Desktop/err2_on_fR2.xlsx')
+
+
+err2_off_f = pd.DataFrame([OFF_2_far[i][2] for i in range(len(OFF_2_far))])
+err2_off_f.columns=['err']
+err2_off_f['abs_err']=abs(err2_off_f['err'])
+err2_off_f['stimulation']='OFF'
+err2_off_f['distance']='far'
+err2_off_f['order']='2nd'
+err2_off_f.to_excel('/home/david/Desktop/err2_off_fR2.xlsx')
+
+
+
+ON_2_close = Parallel(n_jobs = numcores)(delayed(model)(totalTime=3000, targ_onset_1=100, targ_onset_2=1000, angle_target_i=90, presentation_period=100,
+           angle_separation=57, tauE=20, tauI=10,  n_stims=2, I0E=0.05, I0I=0.5, 
+           GEE=0.068*fee,
+           GII= 0.13*fii,
+           GEI=0.13*fei,
+           GIE=0.042*fie, 
+           sigE=7., sigI=5.,  k_noise=0.6,           
+           kappa_E=45, 
+           kappa_I=0.3, 
+           kappa_stim=40., N=512, stim_strengthE=9.4, stim_strengthI=0.,
+           plot_connectivity=False, plot_rate=False, plot_hm=False , plot_fit=False, 
+           phantom_st=1.2, phantom_onset=50000, phnatom_duration=100)  for n in range(n_simuls)) 
+
+
+
+
+OFF_2_close = Parallel(n_jobs = numcores)(delayed(model)(totalTime=3000, targ_onset_1=100, targ_onset_2=1000, angle_target_i=90, presentation_period=100,
+           angle_separation=57, tauE=20, tauI=10,  n_stims=2, I0E=-2., I0I=0.5, 
+           GEE=0.068*fee,
+           GII= 0.13*fii,
+           GEI=0.13*fei,
+           GIE=0.042*fie, 
+           sigE=7., sigI=5., k_noise=0.6,            
+           kappa_E=45, 
+           kappa_I=0.3, 
+           kappa_stim=40., N=512, stim_strengthE=9.4, stim_strengthI=0.,
+           plot_connectivity=False, plot_rate=False, plot_hm=False , plot_fit=False, 
+           phantom_st=1.2, phantom_onset=50000, phnatom_duration=100)  for n in range(n_simuls)) 
+
+
+
+
+
+err2_on_c = pd.DataFrame([ON_2_close[i][2] for i in range(len(ON_2_close))])
+err2_on_c.columns=['err']
+err2_on_c['abs_err']=abs(err2_on_c['err'])
+err2_on_c['stimulation']='ON'
+err2_on_c['distance']='close'
+err2_on_c['order']='2nd'
+err2_on_c.to_excel('/home/david/Desktop/err2_on_cR2.xlsx')
+
+
+err2_off_c = pd.DataFrame([OFF_2_close[i][2] for i in range(len(OFF_2_close))])
+err2_off_c.columns=['err']
+err2_off_c['abs_err']=abs(err2_off_c['err'])
+err2_off_c['stimulation']='OFF'
+err2_off_c['distance']='close'
+err2_off_c['order']='2nd'
+err2_off_c.to_excel('/home/david/Desktop/err2_off_cR2.xlsx')
+
+
+
