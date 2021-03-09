947effe67bb0e6ac6fb9bb8cd94b1b8be0349e6b
David Bestue <davidsanchezbestue@hotmail.com>
2021-03-09 17:49:29 +0100

    update

--- Christos_first_second/classifier_training_set.py
+++ Christos_first_second/classifier_training_set.py
@@ -10,9 +10,9 @@ import multiprocessing
 
 
 N=512
-ch_size = 180
+ch_size = 90
 ch = int(360/ch_size)
-reps_=2
+reps_=5
 
 
 
@@ -57,6 +57,62 @@ outputs_OFF= Parallel(n_jobs = numcores)(delayed(model)(totalTime=1000, targ_ons
 
 X_off = np.array(outputs_OFF).reshape(ch*reps_, N)
 y_off = np.array(Targets)
+
+
+
+
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+from itertools import cycle
+from sklearn import svm, datasets
+from sklearn.metrics import roc_curve, auc
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import label_binarize
+from sklearn.multiclass import OneVsRestClassifier
+from scipy import interp
+from sklearn.metrics import roc_auc_score
+
+X=X_on
+y=y_on
+
+
+# Binarize the output
+y= label_binarize(y, classes=np.arange(0, 360,ch_size))
+n_classes = y.shape[1]
+
+random_state = np.random.RandomState(0)
+n_samples, n_features = X.shape
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.5, random_state=0)
+
+classifier = OneVsRestClassifier(svm.SVC(kernel='linear', probability=True, random_state=random_state))
+
+y_score = classifier.fit(X_train, y_train).decision_function(X_test)
+
+
+# Compute ROC curve and ROC area for each class
+fpr = dict()
+tpr = dict()
+roc_auc = dict()
+for i in range(n_classes):
+    fpr[i], tpr[i], _ = roc_curve(y_test[:, i], y_score[:, i])
+    roc_auc[i] = auc(fpr[i], tpr[i])
+
+# Compute micro-average ROC curve and ROC area
+fpr["micro"], tpr["micro"], _ = roc_curve(y_test.ravel(), y_score.ravel())
+roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
+
+
+
+
+
+
+
+
+
 
 
 
