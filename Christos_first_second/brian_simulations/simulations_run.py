8f8fadd127aba118c0a07ddf2d6e1365ed072278
David Bestue <davidsanchezbestue@hotmail.com>
2021-07-18 13:26:35 +0200

    update

--- Christos_first_second/brian_simulations/simulations_run.py
+++ Christos_first_second/brian_simulations/simulations_run.py
@@ -2,7 +2,29 @@
 
 
 from model import * 
+from joblib import Parallel, delayed
+import multiprocessing
 
-results =  simulation(name_conections='connections_sp.npz', N=1000)
+
+numcores = multiprocessing.cpu_count() 
+if numcores>20:
+    numcores=numcores-10
+if numcores<10:
+    numcores=numcores-3
+
+
+
+### One simulation
+##one_simulation =  simulation(name_conections='connections_sp_30000.npz', N=30000)
+
+
+
+### Multiple simulations in paralel
+extEs = [0,1,2]
+results = Parallel(n_jobs = numcores)(delayed(simulation)(extE=extE, name_conections='connections_sp.npz', N=1000)  for extE in extEs)    
+
+
+
+###io.savemat('/home/david/Desktop/brian_simulations/results_simulation_30000',{'rate':rates, 'spktm': spikes.it})
 
 
