d8c0e68667a4680d552d7e44322f572b20aa7a59
David Bestue <davidsanchezbestue@hotmail.com>
2021-07-19 15:34:21 +0200

    update

--- Christos_first_second/brian_simulations/model.py
+++ Christos_first_second/brian_simulations/model.py
@@ -158,10 +158,19 @@ def simulation(loadconnections=True, name_conections='connections_sp_20000.npz',
   ## If I am not wrong, here I specify the location with this "float(NE)-0.5", being on the center
   ## pos_stim is a fraction where 0 is the first neuron (0) and 1 is the last one(360). 0.5 means stimulation in the middle (180)
   ## 0.75 means stimulating at 90 (1-0.75=0.25; 0.35*360=90). 0.25 means stimulating at 270 (1-0.25=0.75; 0.75*360=270)
+  nstims=1
+  stimat = int(randint(0,nstims)/float(nstims)*NE)
   pos=arange(NE)
-  networkE.Iext=stimE*exp(-0.5*(pos/float(NE)-pos_stim)**2/(epsE**2))#stimE*(1.+epsE*cos(2*pi*(pos/float(NE)-0.5)))
+  inpE=stimE*exp(-0.5*(pos/float(NE)-0.5)**2/(epsE**2))#stimE*(1.+epsE*cos(2*pi*(pos/float(NE)-0.5)))
+  networkE.Iext= np.roll(inpE,stimat-NE/2)
   pos=arange(NI)
-  networkI.Iext=stimI*(1.+epsI*cos(2*pi*(pos/float(NI)-pos_stim)))
+  inpI=stimI*(1.+epsI*cos(2*pi*(pos/float(NI)-0.5)))
+  networkI.Iext= np.roll(inpI,int(stimat*NI/float(NE)-NE/2))
+  ##
+  #pos=arange(NE)
+  #networkE.Iext=stimE*exp(-0.5*(pos/float(NE)-pos_stim)**2/(epsE**2))#stimE*(1.+epsE*cos(2*pi*(pos/float(NE)-0.5)))
+  #pos=arange(NI)
+  #networkI.Iext=stimI*(1.+epsI*cos(2*pi*(pos/float(NI)-pos_stim)))
   run(stim_off-stim_on,report='text')
   ##3rd step of the simulation: delay period
   networkE.Iext=extE
