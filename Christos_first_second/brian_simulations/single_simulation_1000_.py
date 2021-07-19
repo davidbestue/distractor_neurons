155e088139fec872e627b516679520e1adcc732f
David Bestue <davidsanchezbestue@hotmail.com>
2021-07-19 16:53:11 +0200

    updatye

--- Christos_first_second/brian_simulations/single_simulation_1000_.py
+++ Christos_first_second/brian_simulations/single_simulation_1000_.py
@@ -15,13 +15,13 @@ if numcores<10:
 
 
 ## First simulation
-# one_simulation =  simulation2(N=1000, loadconnections=False, saveconnections=True)
-# io.savemat('/home/david/Desktop/brian_simulations/single_simulation',{'spktm': one_simulation})
-
-
-## One simulation
-one_simulation =  simulation2(N=1000, extE=20.*mV)
+one_simulation =  simulation2(N=80000, loadconnections=False, saveconnections=True)
 io.savemat('/home/david/Desktop/brian_simulations/single_simulation',{'spktm': one_simulation})
+
+
+# ## One simulation
+# one_simulation =  simulation2(N=80000, extE=0.24*mV)
+# io.savemat('/home/david/Desktop/brian_simulations/single_simulation',{'spktm': one_simulation})
 
 
 
