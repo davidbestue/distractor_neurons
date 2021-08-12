01dc82e0b760d190454f56d1a70c4d9fd77b2ef6
David Bestue <davidsanchezbestue@hotmail.com>
2021-08-12 10:20:30 +0200

    update

---
+++ Christos_first_second/brian_simulations/simulations_Hansel4_5.py
@@ -0,0 +1,44 @@
+#import brian_no_units
+
+
+from model_Hansel4 import * 
+from joblib import Parallel, delayed
+import multiprocessing
+
+
+numcores = multiprocessing.cpu_count() 
+if numcores>20:
+    numcores=numcores-10
+if numcores<10:
+    numcores=numcores-3
+
+
+
+## One simulation
+#run_simulation(IEext=0., pos_stim=0.5, paralel_simulation=False, path_='/home/david/Desktop/brian_simulations_albert/')
+#one_simulation = run_simulation(IEext=0.2, pos_stim=0.75, save_file=True)
+##one_simulation = run_simulation(IEext=0.2, pos_stim=0.25, save_file=False)
+
+#######################################
+
+### Multiple simulations in paralel
+extEs = [1.25]
+n_ext = len(extEs)
+
+n_=25
+positions = [np.round(np.linspace(0,1,n_)[x],2) for x in range(n_)]
+
+number_ = 10
+
+for extE in extEs:
+	for pos in positions:
+		for n in range(number_):
+			results = run_simulation(IEext=extE, pos_stim=pos, paralel_simulation=False, path_='/home/david/Desktop/brian_simulations_albert/simulations6/')
+
+
+
+
+
+#
+
+
