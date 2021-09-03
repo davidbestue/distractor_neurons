77e193c381ccda5e1ffcdc8763df40ee32ef8b20
David Bestue <davidsanchezbestue@hotmail.com>
2021-09-03 18:19:25 +0200

    update

--- Christos_first_second/brian_simulations/simulations_Hansel5_1.py
+++ Christos_first_second/brian_simulations/simulations_Hansel5_1.py
@@ -13,24 +13,22 @@ if numcores<10:
 
 
 ## One simulation
-## run_simulation(IEext=0., pos_stim=0.5, paralel_simulation=False, path_='/home/david/Desktop/brian_simulations_albert/')
-one_simulation = run_simulation(IEext=0.5, pos_stim=0.25, gee=480.0)
-#one_simulation = run_simulation(IEext=0.2, pos_stim=0.25, save_file=False)
+### one_simulation = run_simulation(IEext=0.5, pos_stim=0.25, gee=480.0)
 
 #######################################
-### Multiple simulations in paralel
-# extEs = [0, 0.5]
-# n_ext = len(extEs)
+## Multiple simulations in paralel
+extEs = [0, 0.5]
+n_ext = len(extEs)
 
-# n_=25
-# positions = [np.round(np.linspace(0,1,n_)[x],2) for x in range(n_)]
+n_=25
+positions = [np.round(np.linspace(0,1,n_)[x],2) for x in range(n_)]
 
-# number_ = 5
+number_ = 5
 
-# for extE in extEs:
-# 	for pos in positions:
-# 		for n in range(number_):
-# 			results = run_simulation(IEext=extE, pos_stim=pos, gee=????????????????????????????, paralel_simulation=False, path_='/home/david/Desktop/brian_simulations_albert/simulations8/')
+for extE in extEs:
+	for pos in positions:
+		for n in range(number_):
+			results = run_simulation(IEext=extE, pos_stim=pos, gee=480.0, paralel_simulation=False, path_='/home/david/Desktop/brian_simulations_albert/simulations8/')
 
 
 
