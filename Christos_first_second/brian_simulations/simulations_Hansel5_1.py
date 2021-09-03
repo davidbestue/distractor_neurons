69333094d842f30dcf585a2c0a88a257b2449657
David Bestue <davidsanchezbestue@hotmail.com>
2021-09-03 16:02:52 +0200

    update

--- Christos_first_second/brian_simulations/simulations_Hansel5_1.py
+++ Christos_first_second/brian_simulations/simulations_Hansel5_1.py
@@ -14,7 +14,7 @@ if numcores<10:
 
 ## One simulation
 ## run_simulation(IEext=0., pos_stim=0.5, paralel_simulation=False, path_='/home/david/Desktop/brian_simulations_albert/')
-one_simulation = run_simulation(IEext=0., pos_stim=0.5, save_file=True)
+one_simulation = run_simulation(IEext=0., pos_stim=0.5)
 #one_simulation = run_simulation(IEext=0.2, pos_stim=0.25, save_file=False)
 
 #######################################
