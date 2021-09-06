594dcc8646270248ca05f24ac2d401b183473007
David Bestue <davidsanchezbestue@hotmail.com>
2021-09-06 18:21:34 +0200

    update

--- Christos_first_second/brian_simulations/simulations_Hansel5_2b.py
+++ Christos_first_second/brian_simulations/simulations_Hansel5_2b.py
@@ -15,6 +15,7 @@ if numcores<10:
 ## One simulation
 #one_simulation = run_simulation(IEext=0.5, pos_stim=0.25, gee=490.0)
 
+
 #######################################
 # Multiple simulations in paralel
 extEs = [0.5]
