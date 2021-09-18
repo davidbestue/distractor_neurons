1a09fa3c7c894495e7a0c92955d486d7d5a2080f
David Bestue <davidsanchezbestue@hotmail.com>
2021-09-18 12:06:33 +0200

    update

--- Christos_first_second/brian_simulations/model_Hansel5.py
+++ Christos_first_second/brian_simulations/model_Hansel5.py
@@ -242,7 +242,6 @@ def run_simulation(IEext=0., pos_stim=0.5, paralel_simulation=False, ix=1, gee=5
 #####################################################################################################
 #                                    RUN SIMULATIONS                                                #
 #####################################################################################################
-
 #Parallel(n_jobs=numcores)(delayed(run_simulation)(i) for i in range(numcores))
 
 ##result = run_simulation(1, IEext=0.2, pos_stim=0.5, save_file=True)
